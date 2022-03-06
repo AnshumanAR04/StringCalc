63c95ef7b4b48d795f5f04b4f05542857d22821e
Singh <I507379@c02g944mmd6r.bbrouter>
2022-03-06 22:25:28 +0530

    Sum for single number test passed

--- src/main/java/StringCalculator/StringCalculator.java
+++ src/main/java/StringCalculator/StringCalculator.java
@@ -5,6 +5,8 @@ public class StringCalculator {
         int sum = 0;
         if( numberList.length() == 0)
             sum = 0;
+        else
+            sum = Integer.parseInt(String.valueOf(numberList.charAt(0)));
         return sum;
     }
 }
--- src/test/java/StringCalculatorTest/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest/StringCalculatorTest.java
@@ -7,8 +7,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StringCalculatorTest {
     @Test
     public void returnZeroOnEmptyCalls() {
-
+        // Test 1: for empty string the addEmbeddedNumbers method should return zero
         int sum = StringCalculator.addEmbeddedNumbers("");
         assertEquals(0, sum);
+    }
+    @Test
+    public void returnNumberForSingleNumber() {
+        // Test 2: for string having 1 number the addEmbeddedNumbers method should return the number
+        int sum = StringCalculator.addEmbeddedNumbers("5");
+        assertEquals(5, sum);
     }
 }
