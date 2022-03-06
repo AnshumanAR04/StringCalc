77414f7a367bb51cdbbc77b6af16997eb88b7a2c
Singh <I507379@c02g944mmd6r.bbrouter>
2022-03-06 16:18:51 +0530

    Empty String Test Failed

--- src/main/java/StringCalculator/StringCalculator.java
+++ src/main/java/StringCalculator/StringCalculator.java
@@ -1,4 +1,7 @@
 package StringCalculator;
 
 public class StringCalculator {
+    public static int add(String s) {
+        return -1;
+    }
 }
--- src/test/java/StringCalculatorTest/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest/StringCalculatorTest.java
@@ -1,4 +1,14 @@
 package StringCalculatorTest;
+import StringCalculator.StringCalculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorTest {
+    @Test
+    public void returnZeroOnEmptuCalls() {
+
+        int ans = StringCalculator.add("");
+        assertEquals(0, ans);
+    }
 }
