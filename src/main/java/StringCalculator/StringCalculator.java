1ca94d531c35d86d4a7fc253939e46a9b9e955ad
AnshumanAR04 <anshumaan.singh17@gmail.com>
2022-03-08 04:23:07 +0530

    The Test for custom delimeters passed

--- src/main/java/StringCalculator/StringCalculator.java
+++ src/main/java/StringCalculator/StringCalculator.java
@@ -3,22 +3,26 @@ package StringCalculator;
 import java.util.List;
 
 public class StringCalculator {
+    // add numbers present in the list
     public static int addNumbers(List<String> numberList) {
         int sum = 0;
         for (String number: numberList) {
+            //convert the String to Integer
             sum += Integer.parseInt(number);
         }
         return sum;
     }
     public static int addEmbeddedNumbers(String numberString) {
+        if(numberString.length() == 0)
+            return 0;
+        //get instance of parser class
         StringParser stringParserObj = StringParser.getInstance();
-
-        String delimeter = stringParserObj.getDelimeter(numberString);
-        List<String> numberList = stringParserObj.getListOfNumbers(numberString, delimeter);
+        //Parse the String for the delimeter
+        stringParserObj.parseDelimeter(numberString);
+        //get list of numbers from the number string
+        List<String> numberList = stringParserObj.getListOfNumbers();
         int sum = addNumbers(numberList);
-
         return sum;
-
     }
 
 }
--- src/main/java/StringCalculator/StringParser.java
+++ src/main/java/StringCalculator/StringParser.java
@@ -3,6 +3,7 @@ package StringCalculator;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringParser {
@@ -10,21 +11,69 @@ public class StringParser {
     private static StringParser stringParserObj = new StringParser();
     // the class has a private constructor so that it cannot be instantiated outside the class
     private StringParser() {}
+    //Pattern to find out if custom delimeter is passed in the string
+    private Pattern changeDelimeterPattern = Pattern.compile("^[/][/]*[\n]*");
+    //Holds the delimeter in use
+    private String delimeter = "";
+    //Store the number string
+    private String numberString = "";
     //get instance method to return the static instance of the class
     public static StringParser getInstance() {
         return stringParserObj;
     }
     // extracts and returns list of numbers based on the delimeter
-    public List<String> getListOfNumbers(String numberString, String delimeter) {
-        Pattern pattern = Pattern.compile("[,\n]");
+    public List<String> getListOfNumbers() {
+        //get the numberString
+        String numberString = getNumberString();
+        //populate the regex pattern for Splitting based on the delimeter
+        Pattern pattern = Pattern.compile(this.delimeter);
         List<String> numberList = new ArrayList<String>();
+        //extract the numbers from the String based on delimeter
         String [] numbers = numberString.split(String.valueOf(pattern));
+        //convert the String Array to an ArrayList
         numberList = Arrays.asList(numbers);
         return numberList;
     }
     // extracts and returns the delimeter from the number String
-    public String getDelimeter(String numberString) {
-        String delimeter = ",";
-        return delimeter;
+    public void parseDelimeter(String numberString) {
+        //set default delimeter
+        delimeter = "[,\n]";
+        //set default number string
+        String numberStringUpdate = numberString;
+        //if a change delimeter line is given then change the numberString and Delimeter Properties
+        if (isChangeDelimeterPassed(numberString)) {
+            //seperate the delimeter and the number string
+            Matcher customDelimeterMatcher = Pattern.compile("//(.)\n(.*)").matcher(numberString);
+            customDelimeterMatcher.matches();
+            //extract the new delimeter symbol
+            String delimeterSymbol = customDelimeterMatcher.group(1);
+            //add option for New Line "\n" to the regular expression
+            delimeter = "[" + delimeterSymbol + "\n]";
+            //update the number string
+            numberStringUpdate = customDelimeterMatcher.group(2);
+        }
+        //set the global properties for delimeter and number string
+        setNumberString(numberStringUpdate);
+        setDelimeter(delimeter);
+    }
+    //setter for delimeter property
+    private void setDelimeter(String delimeter) {
+        this.delimeter = delimeter;
+    }
+    //setter for number string property
+    private void setNumberString(String numberString) {
+        this.numberString = numberString;
+    }
+    //getter for delimeter property
+    public String getDelimeter() {
+        return this.delimeter;
+    }
+    //getter for number string property
+    public String getNumberString() {
+        return this.numberString;
+    }
+    //check if a change delimeter line is provided
+    public boolean isChangeDelimeterPassed(String numberString) {
+        return changeDelimeterPattern.matcher(numberString).find();
     }
 }
